40053b3cdb5e7bfb85fd4360f9ec01ffbcdc2929
Roland Peres <rollyperesh@gmail.com>
2020-03-20 20:04:46 +0200

    Fix imports

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import * as core from '@actions/core'
-const fs = require('fs')
-const path = require('path')
+import * as fs from 'fs'
+import * as path from 'path'
 
 // get input parameter values from config
-var fileName = path.join(process.env.RUNNER_TEMP, core.getInput('fileName'))
+var fileName = path.join(
+  process.env.RUNNER_TEMP || '',
+  core.getInput('fileName')
+)
 
 var encodedString = core.getInput('encodedString')
 
 async function run(): Promise<void> {
   try {
-    console.log(process.env)
+    console.log('ENV', process.env)
     const tempFile = Buffer.from(encodedString, 'base64')
 
     if (tempFile.length == 0) core.setFailed('Temporary file value is not set')
 
-    fs.writeFile(fileName, tempFile, err => {
+    fs.writeFile(fileName, tempFile, (err: any) => {
       if (err) throw err
       console.log('Wrote file!')
     })
